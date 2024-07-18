e36fe5908c724f8cbaf1b070da479de3ae22ba39
Rajvardhan <rajvardhanranawat80@gmail.com>
2024-07-18 11:28:58 +0530

    init 3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-import { LatestPost } from "@/app/_components/post";
+
 import { api, HydrateClient } from "@/trpc/server";
 
 export default async function Home() {
@@ -45,7 +45,7 @@ export default async function Home() {
             </p>
           </div>
 
-          {/* <LatestPost /> */}
+         
         </div>
       </main>
     </HydrateClient>
